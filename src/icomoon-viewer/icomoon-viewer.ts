329d30f9e728a5966fff7c9123d969264555721f
Quique Fdez Guerra <quique@ckgrafico.com>
2018-08-25 16:47:23 +0200

    Small refactor

--- src/icomoon-viewer/icomoon-viewer.ts
+++ src/icomoon-viewer/icomoon-viewer.ts
@@ -1,18 +1,21 @@
-import { window, TextDocument, workspace, Uri, Range } from 'vscode';
+import { window, TextDocument, workspace, Uri, Range, TextEditor, TextEditorDecorationType } from 'vscode';
 import * as glob from 'glob';
 import { Icon } from './icon';
 
 export class IcomoonViewer {
-    private languages = ['css', 'scss', 'less'];
+    private languages = ['css', 'scss', 'sass', 'less'];
     private icons: Icon[];
 
+    private get editor(): TextEditor {
+        return window.activeTextEditor;
+    }
+
+    private get document(): TextDocument {
+        return this.editor.document;
+    }
+
     public async update(): Promise<void> {
-
-        // Get the current text editor
-        let editor = window.activeTextEditor;
-        let doc = editor.document;
-
-        if (!this.languages.includes(doc.languageId)) {
+        if (!this.languages.includes(this.document.languageId)) {
             return;
         }
 
@@ -21,6 +24,36 @@ export class IcomoonViewer {
         }
 
         this.showIcons();
+    }
+
+    private showIcons(): void {
+        this.icons.forEach(icon => {
+            const renderOptions = {
+                before: {
+                    contentIconPath: Uri.parse(`data:image/svg+xml;utf8,${encodeURI(this.svgTemplate(icon))}`),
+                    margin: '0 .1em',
+                    width: '1em',
+                },
+            };
+
+            const decoration = window.createTextEditorDecorationType(renderOptions);
+            const ranges = this.extractIconsFromCSS(icon);
+
+            if (!ranges || ranges.length < 1) {
+                return;
+            }
+
+            this.showDecoration(decoration, ranges);
+        });
+    }
+
+    private showDecoration(decoration: TextEditorDecorationType, ranges: Range[]): void {
+        window.visibleTextEditors
+                .forEach(editor => editor.setDecorations(decoration, ranges));
+    }
+
+    private svgTemplate(icon: Icon): string {
+        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 64 1024 1024"><path transform="rotate(180 512 512) scale(.9)" fill="#FFFFFF" d="${icon.content}"/></svg>`;
     }
 
     private extractIconsFromDoc(doc: TextDocument): Icon[] {
@@ -40,10 +73,10 @@ export class IcomoonViewer {
         return icons;
     }
 
-    private extractIconsFromCSS(doc: TextDocument, icon: Icon): Range[] {
+    private extractIconsFromCSS(icon: Icon): Range[] {
         let ranges: Range[] = [];
-        let docContent = doc.getText();
-        const reg = new RegExp(`${icon.code}`, 'g');
+        let docContent = this.document.getText();
+        const reg = new RegExp(`('|")${icon.code}('|");?`, 'g');
         let match;
 
         while ((match = reg.exec(docContent))) {
@@ -51,7 +84,7 @@ export class IcomoonViewer {
                 return;
             }
 
-            const position = doc.positionAt(match.index + match[0].length + `'.`.length);
+            const position = this.document.positionAt(match.index + match[0].length);
             ranges.push(new Range(position.line, position.character, position.line, position.character));
          }
 
@@ -71,29 +104,6 @@ export class IcomoonViewer {
                 this.icons = this.extractIconsFromDoc(doc);
                 resolve();
             });
-        });
-    }
-
-    private showIcons(): void {
-        // TODO: Separate in functions
-        this.icons.forEach(icon => {
-            const svg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 64 1024 1024"><path transform="rotate(180 512 512) scale(.9)" fill="#FFFFFF" d="${icon.content}"/></svg>`;
-            const renderOptions = {
-                before: {
-                    contentIconPath: Uri.parse(`data:image/svg+xml;utf8,${encodeURI(svg)}`),
-                    width: '1em',
-                },
-            };
-
-            const decoration = window.createTextEditorDecorationType(renderOptions);
-
-            // Get the coords
-            let editor = window.activeTextEditor;
-            const ranges = this.extractIconsFromCSS(editor.document, icon);
-
-            // Show the decoration
-            window.visibleTextEditors
-                .forEach(editor => editor.setDecorations(decoration, ranges));
         });
     }
 
