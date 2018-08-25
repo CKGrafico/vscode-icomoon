71ee3063fe2c9e1698661d2e6c4642fa31914416
Quique Fdez Guerra <quique@ckgrafico.com>
2018-08-25 18:41:39 +0200

    Remove unnecesary helpers

--- src/helpers/numbers.helper.ts
+++
@@ -1,8 +0,0 @@
-export const numbers = {
-    intToHex(int: number): string {
-        return int.toString(16);
-    },
-    hexToInt(hex: string): number {
-        return parseInt(hex, 16);
-    },
-};
